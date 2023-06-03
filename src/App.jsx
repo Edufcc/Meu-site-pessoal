eed8d403c532466ecb1a124314c8e763f3d3664e
Edufcc <edubcc15@gmail.com>
2023-06-03 11:51:55 -0300

    atualiazando experiencia

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,8 @@ function App() {
         cargo: informacao.data.cargo
       });
 
-      const experiencia = await api.get('/experiencia');
-      const habilidade = await api.get('/experiencia');
+      const experiencia = await api.get('/experiencias?tipo=experiencia');
+      const habilidade = await api.get('/experiencias?tipo=habilidade');
       setCurriculo({
         resumo: informacao.data.resumo,
         experiencia: experiencia.data,
