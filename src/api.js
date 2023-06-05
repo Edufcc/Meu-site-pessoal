c0bc4cd3d8277ce81fcdeb9d0e4180f334973fc3
Edufcc <edubcc15@gmail.com>
2023-06-05 20:01:52 -0300

    sem variavel de ambiente

--- src/api.js
+++ src/api.js
@@ -1,6 +1,16 @@
+// import axios from 'axios';
+
+// const apiUrl = import.meta.env.VITE_API_URL
+
+// const api = axios.create({
+//     baseURL: apiUrl,
+// });
+
+// export default api;
+
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL
+const apiUrl = 'https://personal-site-api-gjsh.onrender.com/api'
 
 const api = axios.create({
     baseURL: apiUrl,
