fa5ba15b40b027c410012b71d49132166ad4840b
Edufcc <edubcc15@gmail.com>
2023-08-14 18:36:15 -0300

    fix: profilePic

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header(props) {
 
     return(
         <header>
-                <img src={foto} alt="Foto de Eduardo"/>
+                <img className="profilePic" src={foto} alt="Foto de Eduardo"/>
                 <h1 id="nome-perfil">{nome}</h1>
                 <h3 id="eventoSubtitulo">{cargo}</h3>
         </header>
